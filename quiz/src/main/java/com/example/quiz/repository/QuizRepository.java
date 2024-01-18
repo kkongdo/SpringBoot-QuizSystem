a8a22cb9cd7133f30af799e86b716575af952965
DoHyeonKong <132756641+kkongdo@users.noreply.github.com>
2024-01-18 17:48:54 +0900

    OX퀴즈시스템1
    비즈니스 로직까지 완료

---
+++ quiz/src/main/java/com/example/quiz/repository/QuizRepository.java
@@ -0,0 +1,10 @@
+package com.example.quiz.repository;
+
+import com.example.quiz.entity.Quiz;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuizRepository extends CrudRepository<Quiz, Integer> {
+    @Query("SELECT ID FROM QUIZ where id = 4 ORDER BY ID desc ")
+    Integer getRandomId();
+}
